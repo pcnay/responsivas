e1d0fe598947504d0d51e7db7f99e14929ae63a9
Ramon <pcnay2003@gmail.com>
2021-01-22 05:47:57 +0000

    Se esta elaborando el reporte de cintas

--- controladores/cintas.controlador.php
+++ controladores/cintas.controlador.php
@@ -219,5 +219,5 @@
 
   } // class ControladorCintas
 
-?> 
+ 
   
--- vistas/modulos/reportes/tcpdf/pdf/rep-cintas.php
+++ vistas/modulos/reportes/tcpdf/pdf/rep-cintas.php
@@ -8,7 +8,188 @@ require_once("../../../../../modelos/cintas.modelo.php");
 // Se debe respetar esta alineacion a la izquierda.
 require_once('tcpdf_include.php');
 
+//============================================================+
+// File name   : example_001.php
+// Begin       : 2008-03-04
+// Last Update : 2013-05-14
+//
+// Description : Example 001 for TCPDF class
+//               Default Header and Footer
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com LTD
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
 
+/**
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Default Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ */
+
+
+ // /pdf/tcpdf/reportes/modulos/vistas/controladores/cintas.controlador.php
+
+ class ObtenerCintas
+ {
+	 public function ctrMostrarCintas($item,$valor)
+		 {
+			 $tabla = "t_Cintas";
+			 $respuesta = ModeloCintas::mdlMostrarCintas($tabla,$item,$valor);
+			 return $respuesta;
+ 
+		 } // static public function ctrMostrarCintas()
+ 
+ } // class ObtenerCintas
+
+ 
+ class imprimirCintas
+ {
+ 	 public function traerImpresionCintas()
+	 {
+		 // Imprimir las cintas.
+		 $item = null;
+		 $valor = null;
+		 $respuestaCintas = ObtenerCintas::ctrMostrarCintas($item,$valor);
+ 
+		 //var_dump($respuestasCintas);
+	 }
+
+ }
+
+ $cintas = new imprimirCintas();
+ $cintas->traerImpresionCintas();
+ 
+ $serial = '23232323'; //$respuestaCintas['num_serial'];
+
+// Include the main TCPDF library (search for installation path).
+require_once('tcpdf_include.php');
+
+
+// create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('TCPDF Example 001');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+$pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+	require_once(dirname(__FILE__).'/lang/eng.php');
+	$pdf->setLanguageArray($l);
+}
+
+// ---------------------------------------------------------
+
+// set default font subsetting mode
+$pdf->setFontSubsetting(true);
+
+// Set font
+// dejavusans is a UTF-8 Unicode font, if you only need to
+// print standard ASCII chars, you can use core fonts like
+// helvetica or times to reduce file size.
+$pdf->SetFont('dejavusans', '', 14, '', true);
+
+// Add a page
+// This method has several options, check the source code documentation for more information.
+$pdf->AddPage();
+
+// set text shadow effect
+$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+// Set some content to print
+
+$html = <<<EOD
+		<table>
+		<tr>
+			<td style ="width:150px"><img src="images/logo-negro-bloque.png"></td>
+			<td style="background-color:white; width:140px">
+				<div style="font-size:8.5px; text-align:right; line-height:15px;">
+					<br>
+						$serial
+					<br>
+					Dirección: Calle 44B 92-11
+				</div>
+			</td>
+			<td style="background-color:white; width:140px">
+				<div style="font-size:8.5px; text-align:right; line-height:15px;">
+					<br>
+					Telefono: 300 786 62 49
+					<br>
+					ventas@inventorysystem.com
+				</div>
+			</td>
+			<td style="background-color:white; width:110px; text-align:center; color:red">
+				<br>
+				<br>FACTURA N.<br>$serial</td>
+
+			</td>
+
+		</tr>
+
+		</table>
+
+
+
+
+<h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+<i>This is the first example of TCPDF library.</i>
+<p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+<p>Please check the source code documentation and other examples for further information.</p>
+<p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
+EOD;
+
+// Print text using writeHTMLCell()
+$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+// ---------------------------------------------------------
+
+// Close and output PDF document
+// This method has several options, check the source code documentation for more information.
+$pdf->Output('example_001.pdf', 'I');
+
+//============================================================+
+// END OF FILE
+//============================================================+
+
+
+
+
+
+
+
+/*
 class ObtenerCintas
 {
 	public function ctrMostrarCintas($item,$valor)
@@ -34,14 +215,15 @@ class imprimirCintas
 		$valor = null;
 		$respuestaCintas = ObtenerCintas::ctrMostrarCintas($item,$valor);
 
-		
+		//var_dump($respuestasCintas);
+
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 // Agrega varias páginas para diferentes maquetaciones.
 $pdf->startPageGroup();
 $pdf->AddPage();
-$num_serial = $respuestaCintas['num_serial'];
+$num_serial = '2323232'; 
 
 
 // El primer bloque de la maquetacion
@@ -71,7 +253,7 @@ $bloque1 = <<<EOF
 			</td>
 			<td style="background-color:white; width:110px; text-align:center; color:red">
 				<br>
-				<br>FACTURA N.<br>$respuestaCintas[num_serial]</td>
+				<br>FACTURA N.<br>$num_serial</td>
 
 			</td>
 
@@ -223,7 +405,6 @@ $bloque5 = <<<EOF
 EOF;
 
 $pdf->writeHTML($bloque5,false,false,false,false,'');
-*/
 
 	
 // Para imprimir la factura.
@@ -237,4 +418,5 @@ $pdf->Output('cintas.pdf', 'I');
 $cintas = new imprimirCintas();
 $cintas->traerImpresionCintas();
 
+*/
 
