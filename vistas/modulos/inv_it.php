dfd455d5a3e371c71e4fe8d634beaaf0e78300d4
Ramon <pcnay2003@gmail.com>
2021-12-28 22:11:22 -0800

    Se agrega una ultima modificacion al archivo de * inv-it * y se actualiza el respaldo de la base de datos

--- vistas/modulos/inv_it.php
+++ vistas/modulos/inv_it.php
@@ -521,9 +521,6 @@ function Eliminar_Espacios($cadena)
 						// Verifica si ya existe el serial en la tabla
 						$eXiste_prod = "S";
 						$modelo_sinEspacios = trim($inv_it[7]); // Eliminar_Espacios($inv_it[7]);
-						echo "<br>";
-						echo $inv_it[8].$modelo_sinEspacios;
-
 						$marca_sinEspacios = trim($inv_it[6]); //Eliminar_Espacios($inv_it[6]);
 						$periferico_sinEspacios =trim($inv_it[5]); // Eliminar_Espacios($inv_it[5]);
 
