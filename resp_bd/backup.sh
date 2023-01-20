53cd8e80f7dc9197b04f3f15d03d7dfa705e1978
Ramon <pcnay2003@gmail.com>
2023-01-19 21:03:58 -0800

    Se respalda la base de datos

--- resp_bd/backup.sh
+++ resp_bd/backup.sh
@@ -4,7 +4,7 @@ USER="usuario_responsiva"
 PASSWORD="responsivas-2020"
 DATABASE="bd_responsivas"
 date=$(date +"%Y-%m-%d")
-PATH_OUTPUT="/var/www/html/responsivas/resp_bd/backup"
+PATH_OUTPUT="/var/www/html/Proyectos/responsivas/resp_bd/backup"
 mysqldump --user=$USER --password=$PASSWORD $DATABASE > $PATH_OUTPUT/$DATABASE-$date.sql
 gzip $PATH_OUTPUT/$DATABASE-$date.sql
 
